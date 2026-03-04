940f1507134c6641cbab7af3cf81ab3be6697e12
Taiwo Alli Shola <alli@Taiwos-MacBook-Pro.local>
2026-03-04 11:25:03 +0100

    testing-msg

--- client/src/pages/ChatBox.jsx
+++ client/src/pages/ChatBox.jsx
@@ -10,9 +10,9 @@ import toast from 'react-hot-toast'
 
 const ChatBox = () => {
 
-    const {messages} = useSelector((state)=>state.messages)
-    const {userId} = useParams()
-    const {getToken} = useAuth()
+    const { messages } = useSelector((state) => state.messages)
+    const { userId } = useParams()
+    const { getToken } = useAuth()
     const dispatch = useDispatch()
 
     const [text, setText] = useState('')
@@ -25,7 +25,7 @@ const ChatBox = () => {
     const fetchUserMessage = async () => {
         try {
             const token = await getToken()
-            dispatch(fetchMessages({token, userId}))
+            dispatch(fetchMessages({ token, userId }))
         } catch (error) {
             toast.error(error.message)
         }
@@ -33,16 +33,16 @@ const ChatBox = () => {
 
     const sendMessage = async () => {
         try {
-            if (!text && !image) return 
-            
+            if (!text && !image) return
+
             const token = await getToken()
             const formData = new FormData();
             formData.append('to_user_id', userId);
             formData.append('text', text);
             image && formData.append('image', image);
 
-            const {data} = await api.post('/api/message/send', formData, {
-                headers: {Authorization: `Bearer ${token}`}
+            const { data } = await api.post('/api/message/send', formData, {
+                headers: { Authorization: `Bearer ${token}` }
             })
 
             if (data.success) {
@@ -50,80 +50,94 @@ const ChatBox = () => {
                 setImage(null)
                 dispatch(addMessages(data.message))
                 // await dispatch(fetchMessages({ token, userId }))
-            }else{
+            } else {
                 throw new Error(data.message)
             }
-            
+
         } catch (error) {
             toast.error(error.message)
         }
     }
 
-    useEffect(()=>{
+    // useEffect(()=>{
+    //     fetchUserMessage()
+
+    //     return () =>{
+    //         dispatch(resetMessages())
+    //     }
+    // },[userId])
+    useEffect(() => {
+
         fetchUserMessage()
 
-        return () =>{
+        const interval = setInterval(() => {
+            fetchUserMessage()
+        }, 3000)
+
+        return () => {
+            clearInterval(interval)
             dispatch(resetMessages())
         }
-    },[userId])
 
-    useEffect(()=>{
-        if(connections.length > 0){
+    }, [userId])
+
+    useEffect(() => {
+        if (connections.length > 0) {
             const user = connections.find(connection => connection._id === userId)
             setUser(user)
         }
-    },[connections, userId])
+    }, [connections, userId])
 
     useEffect(() => {
         messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
     }, [messages])
 
-  return user && (
-    <div className='flex flex-col h-screen'>
-        <div className='flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-linear-to-r from-indigo-50 to-purple-50 border-b border-gray-300'>
-            <img src={user.profile_picture} alt="" className='size-8 rounded-full'/>
-            <div>
-                <p className='font-medium'>{user.full_name}</p>
-                <p className='text-sm text-gray-500 -mt-1.5'>@{user.username}</p>
+    return user && (
+        <div className='flex flex-col h-screen'>
+            <div className='flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-linear-to-r from-indigo-50 to-purple-50 border-b border-gray-300'>
+                <img src={user.profile_picture} alt="" className='size-8 rounded-full' />
+                <div>
+                    <p className='font-medium'>{user.full_name}</p>
+                    <p className='text-sm text-gray-500 -mt-1.5'>@{user.username}</p>
+                </div>
             </div>
-        </div>
 
-        <div className='p-5 md:px-10 h-full overflow-y-scroll'> 
-            <div className='space-y-4 max-w-4xl mx-auto'> 
-                {
-                    messages.toSorted((a,b)=> new Date(a.createdAt) - new Date(b.createdAt)).map((message,index)=>(
-                        <div key={index} className={`flex flex-col ${message.to_user_id !== user._id ? 'items-start' : 'items-end'}`}> 
-                            <div className={`p-2 text-sm  bg-white text-slate-700 max-w-sm rounded-lg shadow ${message.to_user_id !== user._id ? 'rounded-bl-none' : 'rounded-br-none'}`}>
-                                {message.message_type === 'image' && <img src={message.media_url} alt="" className='w-full max-w-sm rounded-lg mb-1'/> }
-                                
-                                <p>{message.text}</p>
-                            </div>
-                        </div>
-                    ))
-                }
-                <div ref={messageEndRef} />
-            </div>
-        </div>
-        <div className='px-4'>
-            <div className='flex items-center gap-3 pl-5 bg-white w-full max-w-xl mx-auto border border-gray-300 shadow rounded-full mb-5'>
-                <input type="text" className='flex-1 outline-none text-slate-700' placeholder='Type a message...' onKeyDown={e=>e.key === 'Enter' && sendMessage()} onChange={(e)=>setText(e.target.value)}value={text}/>
-
-                <label htmlFor="image">
+            <div className='p-5 md:px-10 h-full overflow-y-scroll'>
+                <div className='space-y-4 max-w-4xl mx-auto'>
                     {
-                        image ? <img src={URL.createObjectURL(image)} alt="" className='size-8 rounded' />
-                        : <ImageIcon className='size-7 text-gray-400  cursor-pointer'/>
+                        messages.toSorted((a, b) => new Date(a.createdAt) - new Date(b.createdAt)).map((message, index) => (
+                            <div key={index} className={`flex flex-col ${message.to_user_id !== user._id ? 'items-start' : 'items-end'}`}>
+                                <div className={`p-2 text-sm  bg-white text-slate-700 max-w-sm rounded-lg shadow ${message.to_user_id !== user._id ? 'rounded-bl-none' : 'rounded-br-none'}`}>
+                                    {message.message_type === 'image' && <img src={message.media_url} alt="" className='w-full max-w-sm rounded-lg mb-1' />}
+
+                                    <p>{message.text}</p>
+                                </div>
+                            </div>
+                        ))
                     }
-                    <input type="file" id='image' accept='image/*' hidden onChange={(e)=> setImage(e.target.files[0])} />
-                </label>
-
-                <button onClick={sendMessage} className='bg-linear-to-br from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95 rounded-full text-white cursor-pointer'>
-                    <SendHorizonal size={18}/>
-                </button>
+                    <div ref={messageEndRef} />
+                </div>
             </div>
-        </div>
+            <div className='px-4'>
+                <div className='flex items-center gap-3 pl-5 bg-white w-full max-w-xl mx-auto border border-gray-300 shadow rounded-full mb-5'>
+                    <input type="text" className='flex-1 outline-none text-slate-700' placeholder='Type a message...' onKeyDown={e => e.key === 'Enter' && sendMessage()} onChange={(e) => setText(e.target.value)} value={text} />
 
-    </div>
-  )
+                    <label htmlFor="image">
+                        {
+                            image ? <img src={URL.createObjectURL(image)} alt="" className='size-8 rounded' />
+                                : <ImageIcon className='size-7 text-gray-400  cursor-pointer' />
+                        }
+                        <input type="file" id='image' accept='image/*' hidden onChange={(e) => setImage(e.target.files[0])} />
+                    </label>
+
+                    <button onClick={sendMessage} className='bg-linear-to-br from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95 rounded-full text-white cursor-pointer'>
+                        <SendHorizonal size={18} />
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default ChatBox
